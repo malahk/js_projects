0bb4d4b430d6ed7dbd4859209e4f2ccb9c7d4861
Kirill Malah <kirillmalah@gmail.com>
2016-05-03 15:53:07 +0300

    last vchages for data transfer test page

--- js/main.js
+++ js/main.js
@@ -9,21 +9,24 @@
     xhr.open('GET', root + requestUrl, true);
 
     xhr.send();
-
+    document.getElementById('preloader').style.display = 'block';
+    document.getElementById('get-data').setAttribute('disabled', '');
     xhr.onreadystatechange = function() {
       if (xhr.readyState !== 4) return;
 
       if (xhr.status !== 200) {
         alert(xhr.status + ': ' + xhr.statusText);
       } else {
-        generateResultsList(JSON.parse(xhr.response));
+        document.getElementById('preloader').style.display = 'none';
+        document.getElementById('get-data').removeAttribute('disabled');
+        renderResultsList(JSON.parse(xhr.response));
       }
     }
   }
 
   ///////////////////
 
-  function generateResultsList(list) {
+  function renderResultsList(list) {
     if(!list.length) return;
 
     var documentFragment = '';
@@ -38,7 +41,27 @@
   function getListItem(data) {
     if(!data) return;
 
-    return data.name;
+    return '<div class="contact-holder">' +
+      '<div class="personal info">' +
+        '<p><span>Name:</span> ' + data.name + '</p>' +
+        '<p><span>Username:</span> ' + data.username + '</p>' +
+        '<p><span>Email:</span> ' + data.email + '</p>' +
+      '</div>' +
+      '<div class="contacts info">' +
+        '<span id="address">' +
+          'Address:' +
+          '<p><span>Street:</span> ' + data.address.street + '</p>' +
+          '<p><span>Suite:</span> ' + data.address.suite + '</p>' +
+        '</span>' +
+      '</div>' +
+      '<div class="company info">' +
+        '<span id="company">' +
+          'Company:' +
+          '<p><span>Name:</span> ' + data.company.name + '</p>' +
+          '<p><span>Catch Phrase:</span> ' + data.company.catchPhrase + '</p>' +
+        '</span>' +
+      '</div>' +
+    '</div>';
   }
 
   return main;
